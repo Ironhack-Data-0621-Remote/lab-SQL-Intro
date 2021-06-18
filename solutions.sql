30f04ed6b8df000338a86c0220ca48e3c899312a
unknown <tom.dubun@parrot.com>
2021-06-18 13:45:36 +0200

    Solutions for Lab SQL Intro Tom Dubun DAPT

---
+++ solutions.sql
@@ -0,0 +1,14 @@
+-- SELECT * FROM sakila.film;
+-- 3) Select one column from a table. Get film titles
+-- select distinct title from film;
+-- 4) Select one column from a table and alias it. Get unique list of film languages under the alias language. 
+-- select * from sakila.language;
+-- select name as language 
+-- from language;
+-- 5-1) Find out how many stores does the company have?
+-- select count(store_id) from store;
+-- The company has two stores
+-- 5-2) Find out how many employees staff does the company have?
+-- select count(staff_id) from staff;
+-- 5-3) Return a list of employee first names only?
+select first_name from sakila.staff;
