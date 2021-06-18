560cdc5d035990015c014d21ded1b63b414e99f4
KaroD123 <kdiede@gmx.de>
2021-06-18 11:23:54 +0200

    save

---
+++ solutions.sql
@@ -0,0 +1,11 @@
+select title from film;
+
+select distinct language_id as language from film;
+
+select * from store;
+-- it has 2 stores
+
+select * from staff;
+-- 2 employees
+
+select first_name from staff;
