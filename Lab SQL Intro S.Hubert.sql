6628a5688a9f3fe7cb06f9dd1dc2a75f79a6e812
Sophie <84469699+SophieHubert@users.noreply.github.com>
2021-06-18 21:17:03 +0200

    first commit

---
+++ Lab SQL Intro S.Hubert.sql
@@ -0,0 +1,12 @@
+-- select title from FILM ;
+
+-- select (name) as language 
+-- from Language ;
+
+-- select count(store_id)
+-- From Store;
+
+-- select count(staff_id)
+-- from staff ;
+
+-- select first_name from staff;
