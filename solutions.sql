5fff4f71d0f5a240f292af1e4f43e0096d204522
LuisaEsther <luisa.alvan@gmail.com>
2021-06-27 19:49:50 +0200

    labsql1

---
+++ solutions.sql
@@ -0,0 +1,14 @@
+use sakila;
+show tables;
+select * from film;
+select title from film
+limit 50;
+select language_id as lenguaje
+from language
+limit 10;
+select * from language;
+-- they are 2 store
+select * from store;
+select * from staff;
+-- the company has only two employees 
+select first_name, last_name from staff;
